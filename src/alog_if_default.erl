50bc9570ad0b1b80c8a7ae7d0ff7aaa371cc2bed
garry <garry@garry-desktop.(none)>
2011-07-09 14:44:16 +0400

    alog_if_default:log/6 is fixed for nice

--- src/alog_if_default.erl
+++ src/alog_if_default.erl
@@ -4,15 +4,16 @@
 
 -export([get_logs_mod/3, log/6, default_mod_ast/0, default_logsmod_ast/0]).
 
-
 % will return list of loggers for module/tag
 get_logs_mod(_, _, _) ->
     [].
+
 % main logging function
 log(Level, Tag, Module, Line, Format, Args) ->
-    Formatted = [FMod:format(Format, Args) || FMod <- get_logs_mod(Level, Module, Tag)], 
-    [LMod:log(Level, Tag, Module, Line, FEver) || LMod <- get_logs_mod(Level, Module, Tag), 
-						  FEver <- Formatted].
+        [begin                          
+         Formatted = Mod:format(Format, Args, Tag, Module, Line),
+         Mod:log(Level, Formatted)   
+	 end || Mod <- get_logs_mod(Level, Module, Tag)].
 
 % wille return default AST of this module after parse_transform
 default_mod_ast() ->
