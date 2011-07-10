5091bb3268be40da955340e6a264285d0bc2220a
Groshev Dmitry <a2alt@ya.ru>
2011-07-10 15:27:20 +0400

    removed alog_app.erl file, alog.erl is now app callback

--- src/alog.erl
+++ src/alog.erl
@@ -1,7 +1,10 @@
 -module(alog).
+-behaviour(application).
 
 %% API
 -export([start/0, stop/0]).
+%% Application callbacks
+-export([start/2, stop/1]).
 
 %%% API
 start() ->
@@ -10,3 +13,12 @@ start() ->
 
 stop() ->
     application:stop(alog).
+
+%% Application callbacks
+start(_StartType, _StartArgs) ->
+    Link = alog_sup:start_link(),
+    alog_examples:run_examples(),
+    Link.
+
+stop(_State) ->
+    ok.
