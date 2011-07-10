0354c431beb605112ed6a5abdff3c7732201470a
garry <garry@garry-desktop.(none)>
2011-07-10 13:25:23 +0400

    alog_parse_trans priority as list, integer, tuple

--- src/alog_parse_trans.erl
+++ src/alog_parse_trans.erl
@@ -1,3 +1,12 @@
+%%%----------------------------------------------------------------------
+%%% File    : alog_parse_trans.erl
+%%% Author  : Artem Golovinsky <artemgolovinsky@gmail.com>
+%%% Purpose :
+%%% Created : 09 Jul 2011 by Artem Golovinsky <artemgolovinsky@gmail.com>
+%%%
+%%% alogger, Copyright (C) 2011  Siberian Fast Food
+%%%----------------------------------------------------------------------
+
 -module(alog_parse_trans).
 
 -export([parse_transform/2, load_config/1]).
@@ -22,7 +31,7 @@ load_config(Config) ->
 	    try load_config2(NewAst) of
 		Val ->
 		    Val
-	    catch
+	    catch 
 		Class:Exp ->
 		    {error, Class, Exp}
 	    end;
@@ -37,9 +46,9 @@ load_config2(NewAst) ->
     _Source = erl_prettypr:format(erl_syntax:form_list(NewAst)),
     {ok, ModuleName, Bin} = compile:forms(NewAst),
     code:load_binary(ModuleName, ?IFACE_SOURCE, Bin).
-
+	
 make_ast(Config) ->
-    case check_config(Config) of
+    case check_config(Config) of 
 	ok ->
 	    make_proceed_ast(Config);
 	Other ->
@@ -61,9 +70,9 @@ multiply_clauses([{{What,Mods},Prio, Loggers}|Configs], Acc) ->
 multiply_clauses([], Acc) ->
     Acc.
 
-make_clause(What, [Mod|Mods], {Guard,Pri}, Loggers, Acc)  ->
+make_clause(What, [Mod|Mods], Prio, Loggers, Acc)  ->
     AbsLogs = [abstract(Loggers)],
-    make_clause(What,Mods, {Guard,Pri}, Loggers,[{clause, 0, get_arity(What,Mod),[get_guard(Guard, Pri)],AbsLogs}|Acc]);
+    make_clause(What,Mods, Prio, Loggers,[{clause, 0, get_arity(What,Mod),get_guard(Prio),AbsLogs}|Acc]);   
 make_clause(_,[], _, _, Acc) ->
     Acc.
 
@@ -76,14 +85,14 @@ insert_clauses([], _Ast) ->
     [].
 
 insert_clauses_every({function,Line,get_mod_logs,Arity,_Clause}, Clauses) ->
-    {function,Line,get_mod_logs,Arity,Clauses};
+    {function,Line,get_mod_logs,Arity,Clauses};    
 insert_clauses_every(Any, _Clauses) ->
     Any.
 
-% Check config.
+% Check config. 
 
-check_config([{{mod,Mods},Prio, Loggers}|Configs]) ->
-    case is_loaded(Mods, Prio, Loggers) of
+check_config([{{mod, _},Prio, Loggers}|Configs]) ->
+    case is_loaded(Prio, Loggers) of
 	ok ->
 	    check_config(Configs);
 	Other ->
@@ -103,15 +112,12 @@ check_config([]) ->
 
 % Modules are loaded?
 
-is_loaded(Mods, Prio, Loggers) ->
-    is_loaded(Prio, Mods ++ Loggers).
-
 is_loaded(Prio, AllMods) ->
     Loaded = lists:filter(fun(Elem) ->
-				  case code:is_loaded(Elem) of
-				      {file, _} ->
+				  case code:ensure_loaded(Elem) of
+				      {module, _} ->
 					  false;
-				      false ->
+				      {error, _} ->
 					  true
 				  end
 			   end, AllMods),
@@ -125,6 +131,18 @@ is_loaded(Prio, AllMods) ->
 
 % check of guards :-)
 
+check_prio([P|All]) ->
+    case check_prio(P) of 
+	ok ->
+	    check_prio(All);
+	Other ->
+	    Other
+    end;
+check_prio([]) ->
+    ok;
+check_prio(Level) when is_integer(Level), 
+		      Level >= ?emergency, Level =< ?debug ->
+    ok;
 check_prio({'>', Level}) when Level >= ?emergency, Level =< ?debug ->
     ok;
 check_prio({'<', Level}) when Level >= ?emergency, Level =< ?debug ->
@@ -142,23 +160,34 @@ check_prio(Other) ->
 
 % Compose new AST for get_mod_logs/3
 
-get_arity(mod,Mod) ->
+get_arity(mod,Mod) -> 
     [{var,0,'Level'},{atom,0,Mod},{var,0,'Tag'}];
-get_arity(tag,Tag) ->
+get_arity(tag,Tag) -> 
     [{var,0,'Level'},{var,0,'Module'},{atom,0,Tag}].
-get_guard(G, Level) ->
-    [{op,0,G,{var,0,'Level'},{integer,0, Level}}].
-% ---------------------------------
+
+get_guard(Prio) when is_list(Prio) ->
+    get_guard(Prio, []);
+get_guard(Prio) when is_integer(Prio);is_tuple(Prio) ->
+    [get_guard_low(Prio)].
+get_guard([G|Gs], Acc) ->
+    get_guard(Gs, [get_guard_low(G)|Acc]);
+get_guard([], Acc) -> Acc.
+
+get_guard_low({G, Level}) ->
+    [{op,0,G,{var,0,'Level'},{integer,0, Level}}];
+get_guard_low(Pri) when is_integer(Pri) ->
+    [{op,0,'=:=',{var,0,'Level'},{integer,0,Pri}}].
+% ---------------------------------   
 def_clause() ->
     {function,_Line,get_mod_logs,_Arity,DefCl} = alog_if_default:default_modlogs_ast(),
     DefCl.
 
 % --------------------------
-% Works during first compilation. AST of alog_if is written to alog_is_default:default_mod_ast/0
+% Works during first compilation. AST of alog_if is written to alog_is_default:default_mod_ast/0 
 make_default_ast(Forms) ->
-    ModAst    = abstract(change_and_remove(Forms)),
+    ModAst = abstract(change_and_remove(Forms)),
     Glm = abstract(find_gml(Forms)),
-    mdma_transform(Forms, ModAst, Glm).
+    mdma_transform(Forms, ModAst, Glm). 
 
 find_gml([{function,Line,get_mod_logs,Arity,Clause}|_]) ->
     {function,Line,get_mod_logs,Arity,Clause};
@@ -174,7 +203,7 @@ mdma_transform([F|Fs], Ast, Glm) ->
     Fs1 = mdma_transform(Fs, Ast, Glm),
     [F1|Fs1];
 mdma_transform([], _Ast, _Glm) -> [].
-
+  
 mdma_transform_every({function,Line,default_mod_ast,Arity,Clause}, Ast, _Glm) ->
     NewClause = transform_clause(Clause, Ast),
     {function,Line,default_mod_ast,Arity,NewClause};
@@ -190,10 +219,10 @@ mdma_transform_every(Node, _Ast, _Glm) ->
 
 change_and_remove([{function,_,default_modlogs_ast,_,_}|Fs]) ->
     Fs1 = change_and_remove(Fs),
-    Fs1;
+    Fs1;    
 change_and_remove([{function,_,default_mod_ast,_,_}|Fs]) ->
     Fs1 = change_and_remove(Fs),
-    Fs1;
+    Fs1; 
 change_and_remove([F|Fs]) ->
     F1 = cmd_every(F),
     Fs1 = change_and_remove(Fs),
@@ -201,11 +230,11 @@ change_and_remove([F|Fs]) ->
 change_and_remove([]) -> [].
 
 cmd_every({attribute,_,file,{_,_}}) ->
-    {attribute,1,file,{?IFACE_SOURCE,1}};
+    {attribute,0,file,{?IFACE_SOURCE,0}};
 cmd_every({attribute,_,module,_}) ->
     {attribute,1,module,?IFACE_MODE};
 cmd_every({attribute,_, export, Funcs}) ->
-    F1 = proplists:delete(default_mod_ast, Funcs),
+    F1 = proplists:delete(default_mod_ast, Funcs), 
     F2 = proplists:delete(default_modlogs_ast, F1),
     {attribute,1, export, F2};
 
