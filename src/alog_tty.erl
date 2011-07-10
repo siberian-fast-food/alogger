562c1edf8c40fb063224889c5f36f19966bc632d
Alexander Dergachev <alexander.dergachev@gmail.com>
2011-07-10 10:02:50 +0200

    Add simple log interface alog_tty.erl

---
+++ src/alog_tty.erl
@@ -0,0 +1,56 @@
+-module(alog_tty).
+-author('alexander.dergachev@gmail.com').
+
+-behaviour(gen_alogger).
+
+-export([ start/1
+        , stop/0
+        , log/2
+        , format/6]).
+
+%%%----------------------------------------------------------------------
+%%% @spec start(SupRef::term()) -> ok
+%%%
+%%% @doc
+%%% @end
+%%%----------------------------------------------------------------------
+-spec start(term()) -> ok.
+
+start(_) ->
+    ok.
+
+%%%----------------------------------------------------------------------
+%%% @spec stop() -> ok
+%%%
+%%% @doc
+%%% @end
+%%%----------------------------------------------------------------------
+-spec stop() -> ok.
+
+stop() ->
+    ok.
+
+%%%----------------------------------------------------------------------
+%%% @spec log(ALoggerPrio::integer(), Msg::string()) -> ok
+%%%
+%%% @doc
+%%% @end
+%%%----------------------------------------------------------------------
+-spec log(integer(), string()) -> ok.
+
+log(_ALoggerPrio, Msg) ->
+    io:format(lists:flatten(Msg), []).
+
+%%%----------------------------------------------------------------------
+%%% @spec format(FormatString::string(), [term()], Tag::string(),
+%%%              Module::atom(), Line::integer(), Pid::pid()) -> io_list()
+%%%
+%%% @doc returns formated log message
+%%% @end
+%%%----------------------------------------------------------------------
+-spec format(string(), [term()], string(),
+             atom(), integer(), pid()) -> iolist().
+
+format(FormatString, Args, Tag, Module, Line, Pid) ->
+    alog_common_formater:format(FormatString, Args,
+                                Tag, Module, Line, Pid).
